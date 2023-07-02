611b79f24709580beac0ac02bbd73c9ed1875b9d
Danny <danwblackburn@me.com>
2023-07-02 06:43:34 +0100

    Add data logging

--- obd.py
+++ obd.py
@@ -1,9 +1,26 @@
 import RPi.GPIO as GPIO
 
-channel = 21
+import time
+import csv
+from datetime import datetime, timezone
+
+recording_data = False
+
+start_time = time.time()
+
+channel = 40
 
 GPIO.setmode(GPIO.BOARD)
-GPIO.setup(channel, GPIO.IN)
+GPIO.setup(channel, GPIO.IN, pull_up_down=GPIO.PUD_DOWN)
+
+if recording_data:
+	filename = "logs/{now}.csv".format(now = datetime.now(timezone.utc).strftime('%y%m%d %H:%M:%S'))
+	logger = csv.writer(open(filename, 'w'))
+	logger.writerow(['time', 'reading'])
 
 while True:
-	print(GPIO.input(channel))
+	reading = GPIO.input(channel)
+	if recording_data:
+		logger.writerow([time.time(), reading])
+	time.sleep(.001)
+	
