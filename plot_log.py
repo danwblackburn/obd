167bd8085ca37c539366b42e74f84bb733c0030e
Danny B <danwb@stanford.edu>
2023-07-02 01:24:53 -0700

    Add plotting from stdin

--- plot_log.py
+++ plot_log.py
@@ -4,16 +4,33 @@ import numpy as np
 import sys
 import csv
 
-print(sys.argv[1])
+if(len(sys.argv) > 1):
+    print(sys.argv[1])
+    file = open(sys.argv[1])
+    reader = csv.reader(file, delimiter=',')
+    headers = next(reader)
+    file.close
 
-file = open(sys.argv[1])
-reader = csv.reader(file, delimiter=',')
-headers = next(reader)
-file.close
+    data = np.genfromtxt(sys.argv[1], delimiter=",", dtype=float, skip_header=1)
 
-data = np.genfromtxt(sys.argv[1], delimiter=",", dtype=float, skip_header=1)
+    time = data[:, 0]
+    reading = data[:, 1]
+    plt.plot(time, reading)
+    plt.show()
+else:
+    fig = plt.figure()
+    fig.show()
 
-time = data[:, 0]
-reading = data[:, 1]
-plt.plot(time, reading)
-plt.show()
+    times = []
+    readings = []
+    while True:
+        line = sys.stdin.readline() 
+        if line == '':
+            break
+       
+        time, reading = line.split(',')
+        times.append(time)
+        readings.append(reading)
+        plt.plot(times, readings)
+        plt.draw()
+    
